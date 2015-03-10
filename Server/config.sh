1f364ac6a2340eb02b887e6946c401b024383561
Mat Elder <mat@n00bworks.com>
2015-03-10 19:50:21 -0400

    compact sh

--- Server/config.sh
+++ Server/config.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 apt-get update &> /dev/null
-apt-get install sqlite3 libsqlite3-dev -y &> /dev/null
-apt-get install php5-fpm php5-sqlite -y &> /dev/null
-apt-get install nginx -y &> /dev/null
+apt-get install sqlite3 libsqlite3-dev php5-fpm php5-sqlite nginx -y &> /dev/null
 cp /vagrant/server/collector /etc/nginx/sites-available/collector &> /dev/null
 ln -s /etc/nginx/sites-available/collector /etc/nginx/sites-enabled/
 rm -rf /etc/nginx/sites-enabled/default
